c49031b02d02c9fdd153ee2f27ed306923e1e903
Pavel Sharko <pavel.sharko@mail.ru>
2025-03-03 22:50:00 +0300

    classNames

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import { Theme, ThemeContext } from "./theme/ThemeContext";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames";
 
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
