654c6a5bc76781818ff2f8ba436a877fe3be6b83
Pavel Sharko <pavel.sharko@mail.ru>
2025-03-11 00:05:23 +0300

    FSD 2.0

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
-import {AboutPageLazy} from "../pages/AboutPage/AboutPage.lazy";
-import {MainPageLazy} from "../pages/MainPage/MainPage.lazy";
+import { AboutPage } from '../pages/AboutPage';
+import {MainPage} from "../pages/MainPage";
 
 
 
-import {classNames} from "../helpers/classNames"
+import {classNames} from "../helpers/classNames/classNames"
 import {useTheme} from "../app/providers/ThemeProvider"
 
 
@@ -20,8 +20,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageLazy />}/>
-                    <Route path={'/'} element={<MainPageLazy />}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>
         </div>
---
+++ src/pages/MainPage/index.ts
@@ -0,0 +1,4 @@
+
+import { MainPageLazy } from "./ui/MainPage.lazy"
+export {MainPageLazy as MainPage,
+}
