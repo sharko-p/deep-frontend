2d61fc3f3817ada197ec97b94c756f6c327789b6
Pavel Sharko <pavel.sharko@mail.ru>
2025-03-23 22:33:50 +0300

    svg/file loaders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,14 +9,14 @@ import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
 
      <Navbar/>
       <AppRouter/>
-      <button onClick={toggleTheme}>TOGGLE</button>
+     
     </div>
   );
 };
