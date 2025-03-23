5ffd3c155916afd9d2898edb5411ab89f0934ecb
Pavel Sharko <pavel.sharko@mail.ru>
2025-03-23 23:41:58 +0300

    sidebar1.0

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
-
 import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
-
 
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "../widgets/Sidebat/ui";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-
-     <Navbar/>
-      <AppRouter/>
-     
+      <Navbar />
+      <div className="content-page ">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
