6b700a946df37a0c214a9db147ee8f69fcefcc9e
Pavel Sharko <pavel.sharko@mail.ru>
2025-04-11 23:17:34 +0300

    add unit test 1.2

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,8 +1,7 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
