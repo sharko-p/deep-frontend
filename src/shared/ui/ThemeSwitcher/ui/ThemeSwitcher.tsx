9dec2290dfc536f73222e47a29ba2e69dfdc020a
Pavel Sharko <pavel.sharko@mail.ru>
2025-04-11 23:16:25 +0300

    add unit test 1.1

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,12 +1,11 @@
 import {
-    classNames
+    classNames,
 } from 'shared/lib/classNames/classNames';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-
 
 interface ThemeSwitcherProps {
     className?: string;
