bba0a07590b901979cec9cfb4bc7b61fcd13c8f0
Pavel Sharko <pavel.sharko@mail.ru>
2025-06-11 23:24:24 +0300

    RTK 1.2 counter

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,32 +1,36 @@
 import { Button } from 'shared/ui/Button/Button';
 import { useDispatch, useSelector } from 'react-redux';
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { useTranslation } from 'react-i18next';
 import { counterActions } from '../model/slice/counterSlice';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
     const dispatch = useDispatch();
-    const counterValue = useSelector((state:StateSchema) => state);
+    const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
+
     const increment = () => {
         dispatch(counterActions.increment());
     };
+
     const decrement = () => {
         dispatch(counterActions.decrement());
     };
+
     return (
         <div>
-            <h1>
-                value=
-                {counterValue}
-            </h1>
+            <h1 data-testid="value-title">{counterValue}</h1>
             <Button
-                onClick={() => { increment; }}
+                onClick={increment}
+                data-testid="increment-btn"
             >
-                increment
+                {t('increment')}
             </Button>
             <Button
-                onClick={() => { decrement; }}
+                data-testid="decrement-btn"
+                onClick={decrement}
             >
-                decrement
+                {t('decrement')}
             </Button>
         </div>
     );
