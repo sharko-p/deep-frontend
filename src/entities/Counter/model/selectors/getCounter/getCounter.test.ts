1f116d503f69fe3eb1650adb0d58be3a90d13f1a
Pavel Sharko <pavel.sharko@mail.ru>
2025-06-26 22:53:17 +0300

    RTK 2.0 test

--- src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
+++ src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
@@ -1,14 +1,13 @@
-import {
-    getCounter
-} from "./getCounter";
-import {DeepPartial} from "@reduxjs/toolkit";
-import {StateSchema} from "app/providers/StoreProvider";
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getCounter } from './getCounter';
 
-discribe('getCounter', ()=>{
-    test('should return counter value',()=>{
-        const state: DeepPartial<StateSchema>={
-            counter: {value: 10},
-        }
-        expect((getCounter(state as StateSchema)).toEqual({value: 10}))
-    })
-})
+describe('getCounter', () => {
+    test('should return counter value', () => {
+        const state: DeepPartial<StateSchema> = {
+            counter: { value: 10 },
+        };
+        expect(getCounter(state as StateSchema)).toEqual({ value: 10 });
+    });
+});
+
